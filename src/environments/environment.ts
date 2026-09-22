6f83cc9d7693064f82aed498cf8db79e26a920bf
joferdev2022 <fernando.lovaton.lopez@gmail.com>
2026-09-22 12:46:16 -0500

    preparado para desplegar2

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   // El proxy de desarrollo y el futuro servidor web deben dirigir /api a FastAPI.
-  apiUrl: '/api/v1/public'
+  apiUrl: 'https://api-encuesta.munitingomaria.gob.pe/api/v1/public'
 };
